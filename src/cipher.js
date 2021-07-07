2f1295df70e98868945b05dc4dd8b67e55977243
nandiaz <nancy.090495@gmail.com>
2021-07-07 18:46:39 -0500

    primerCommit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,55 @@
-const cipher = {
-  // ...
+import {resultDecrypt, resultEncrypt} from './index.js';
+let cipher = {
+    encode: (text, usseroffset) => {
+      //Para sacar de ascii a letra
+      let ascciToLetter;
+      for (let i = 0; i < text.length; i++) {
+        let ascii = text.charCodeAt(i);
+        if (ascii >= 65 && ascii <= 90) {
+          //MAYUSCULAS
+          let formulamayus = ((ascii - 65 + usseroffset) % 26 + 65);
+          resultEncrypt[i] = formulamayus;
+        
+
+        } else if (ascii >= 97 && ascii <= 122) {
+          let formulaminus = ((ascii - 97 + usseroffset) % 26 + 97);
+          resultEncrypt[i]= formulaminus;
+
+        } else {
+          //CUALQUIER OTRO CARACTER SE QUEDA IGUAL
+          let formulaAscii = ascii;
+          resultEncrypt[i] = formulaAscii;
+        } 
+      }
+      //Imprimir a letra
+      for (let j = 0; j < resultEncrypt.length; j++){
+      ascciToLetter= String.fromCharCode(resultEncrypt[j]);
+      resultEncrypt[j] = ascciToLetter;
+      }
+      return resultEncrypt;
+    },
+
+    decode: (resultEncrypt, usseroffset) => {      
+      for (let k= 0; k < resultEncrypt.length; k++) {
+        let ascii2 = resultEncrypt[k].charCodeAt();
+        
+        if (ascii2 <= 91 && ascii2 >= 61) {
+          let formuladecif = (((ascii2 + 65) - usseroffset) % 26 + 65);
+          resultDecrypt[k] = formuladecif;
+        } else if (ascii2 <= 122 && ascii2 >= 95) {
+          let formuladecifminus= (((ascii2 - 122) - usseroffset) % 26 + 122);
+          resultDecrypt[k] = formuladecifminus;
+        } else {
+          let formulaAscii2 = ascii2;
+          resultDecrypt[k] = formulaAscii2;
+        }
+      }
+      for (let l = 0; l < resultDecrypt.length; l++){
+       let ascciToLetter2 = String.fromCharCode(resultDecrypt[l]);
+       resultDecrypt[l] = ascciToLetter2;
+      }
+      return resultDecrypt;
+    }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import cipher from './cipher.js';
+import {default as cipher} from './cipher.js';
 
-console.log(cipher);
+//window.addEventListener("load", inicio, true);
+
+  //function inicio () {
+    //document.getElementById("mensaje").addEventListener("keyup",function(){
+      //this.value = this.value.toUpperCase();
+    //});
+  //}
+let offset = document.getElementById ('selectoffset');
+let text = document.getElementById ('mensaje');
+let button1 = document.getElementById('buttoncipher');
+let button2 = document.getElementById ('buttondecipher');
+let printresult = document.getElementById ('mensaje2');
+let resultEncrypt= [];
+let resultDecrypt= [];
+
+button1.addEventListener ('click' , () => {
+    let textValue = text.value;
+    let offsetValue = (parseInt(offset.value));
+    resultEncrypt.toString;
+    cipher.encode(textValue, offsetValue);
+    printresult.innerHTML = resultEncrypt.join('');
+
+});
+
+button2.addEventListener ('click', () => {
+    let offsetValue = parseInt(offset.value);
+    resultEncrypt.toString;
+    cipher.decode(resultEncrypt, offsetValue);
+    printresult.innerHTML = resultDecrypt.join("");
+    
+});
+export {resultDecrypt, resultEncrypt}
